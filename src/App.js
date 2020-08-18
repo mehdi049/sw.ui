07c3acf0697a09be10937f6b40ee52b48e72c835
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2020-08-18 14:50:20 +0100

    - download required packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./utils/History";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router history={history}>
+        <Header />
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
