69acf6988a97c34bb151a21b3d735f576744f023
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2020-09-28 13:10:50 +0100

    - add toast to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import HeaderCategories from "./_sharedComponents/HeaderCategories";
+import ToastMessage from "./_sharedComponents/ToastMessage";
 function Contact() {
+  const [toastMessage, setToastMessage] = useState();
+  const [displayToast, setDisplayToast] = useState(false);
+  const [toastType, setToastType] = useState();
+
+  /** toast function */
+  function toastHandler(message, type) {
+    setDisplayToast(true);
+    setToastMessage(message);
+    setToastType(type);
+  }
+
+  function contactHandler() {
+    toastHandler("Message envoyé avec succé", "success");
+  }
+
   return (
     <>
       <HeaderCategories />
@@ -23,12 +39,20 @@ function Contact() {
                 <Form.Label className="dark-blue">Adresse</Form.Label>
                 <Form.Control as="textarea" rows="3" />
               </Form.Group>
-              <Button variant="primary" type="submit">
+              <Button variant="primary" type="button" onClick={contactHandler}>
                 Envoyer
               </Button>
             </Form>
           </Col>
         </Row>
+
+        <ToastMessage
+          show={displayToast}
+          toastHandler={toastHandler}
+          onClose={() => setDisplayToast(false)}
+          message={toastMessage}
+          type={toastType}
+        />
       </Container>
     </>
   );
